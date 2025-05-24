262eb1917317a67c98ae84e70344a9fd7a9d4b7f
Charles Johnson <charlesjohnson@Charless-MacBook-Pro-2.local>
2025-05-23 23:55:06 -0400

    Fix: Finalize next.config.mjs for Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 import { withContentlayer } from "next-contentlayer";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,7 +16,7 @@ const nextConfig = {
     serverComponentsExternalPackages: ["@prisma/client"]
   },
   webpack(config) {
-    config.resolve.alias["@" ] = path.resolve(__dirname);
+    config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   }
 };

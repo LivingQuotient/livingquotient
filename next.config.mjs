3363567b84d0c26323ee3d0c537d9ad1423df318
Charles Johnson <charlesjohnson@Charless-MacBook-Pro-2.local>
2025-05-23 23:52:27 -0400

    Final alias and config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,19 @@
-import { withContentlayer } from "next-contentlayer"
-
-// import "./env.mjs"
+import { withContentlayer } from "next-contentlayer";
+import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    domains: ["avatars.githubusercontent.com"]
   },
   experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["@prisma/client"],
+    serverComponentsExternalPackages: ["@prisma/client"]
   },
-}
+  webpack(config) {
+    config.resolve.alias["@" ] = path.resolve(__dirname);
+    return config;
+  }
+};
 
-export default withContentlayer(nextConfig)
+export default withContentlayer(nextConfig);

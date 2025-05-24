d0f24e3e1b85d4c12a6e4194ee3856cb2667c668
Charles Johnson <charlesjohnson@Charless-MacBook-Pro-2.local>
2025-05-24 00:20:04 -0400

    re-copy original repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,17 @@
-import { withContentlayer } from 'next-contentlayer'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import { withContentlayer } from "next-contentlayer"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import "./env.mjs"
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ['@xata.io/client']
+  images: {
+    domains: ["avatars.githubusercontent.com"],
   },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
-    return config
-  }
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
 }
 
 export default withContentlayer(nextConfig)
